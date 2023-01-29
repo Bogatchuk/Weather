a07f1ab5cb475a43daadd5f9f921a5d1e91b8659
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2023-01-29 23:26:31 +0300

    fixed bug with alert

--- Weather/Extensions/UIViewController+Extensions.swift
+++ Weather/Extensions/UIViewController+Extensions.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 extension UIViewController {
-    func showAlert(withTitle title: String, withMessage message:String) {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let ok = UIAlertAction(title: "OK", style: .default, handler: { action in
-            })
-            let cancel = UIAlertAction(title: "Cancel", style: .default, handler: { action in
-            })
-            alert.addAction(ok)
-            alert.addAction(cancel)
-            DispatchQueue.main.async(execute: {
-                self.present(alert, animated: true)
-            })
-        }
+    func showAlert(withTitle title: String, withMessage message:String, completion: (() -> ())? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { action in
+            completion?()
+        })
+        alert.addAction(ok)
+        DispatchQueue.main.async(execute: {
+            self.present(alert, animated: true)
+        })
+    }
 }
